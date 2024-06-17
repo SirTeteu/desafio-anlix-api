0e176a484cdc36386923db23ffb78ee57ae7a69d
Matheus Fernandes <matheusfcc2012@gmail.com>
2024-06-17 00:00:11 -0300

    adding paciente relationships

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Paciente extends Model
 {
     public $timestamps = false;
+
+    public function cardiacoIndices() {
+        return $this->hasMany(IndiceCardiaco::class);
+    }
+
+    public function pulmonarIndices() {
+        return $this->hasMany(IndicePulmonar::class);
+    }
+
+    public function latestCardiacoIndice() {
+        return $this->hasOne(IndiceCardiaco::class)
+            ->orderBy('data', 'desc')
+            ->orderBy('id', 'desc');
+    }
+
+    public function latestPulmonarIndice() {
+        return $this->hasOne(IndicePulmonar::class)
+            ->orderBy('data', 'desc')
+            ->orderBy('id', 'desc');
+    }
 }
