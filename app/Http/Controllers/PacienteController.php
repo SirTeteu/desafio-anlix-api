9228e462ab91ff3fe3a934c6fc9292dd6ccea261
Matheus Fernandes <matheusfcc2012@gmail.com>
2024-06-17 00:09:48 -0300

    adding paciente index endpoint with nome, indice cardiaco and indice pulmonar filters

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PacienteResource;
+use App\Models\Paciente;
+use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Lista vários pacientes com filtro de nome (opcional)
+     * Se acrescentar os parâmetros latest-cardiaco-indice
+     * ou latest-pulmonar-indice, acrescentará na resposta
+     * o respectivo indice mais recente de cada paciente
+     *
+     * @param Request $request
+     * @return PacienteResource
+     */
+    public function index(Request $request) {
+        $pacientes = Paciente::query();
+
+        // filtrando pacientes pelo nome
+        if($request->has('nome')) {
+            $nome = $request->input('nome');
+
+            $pacientes->where('nome', 'like', "%$nome%");
+        }
+
+        // se o paramentro latest-cardiaco-indice for passado na request
+        // entao acrescenta na resposta o indice cardiaco mais recente
+        if ($request->has('latest-cardiaco-indice') && 
+            $request->input('latest-cardiaco-indice') == 1) {
+            
+                $pacientes = $pacientes->with('latestCardiacoIndice');
+        }   else if ($request->has('latest-pulmonar-indice') && 
+            $request->input('latest-pulmonar-indice') == 1) {
+                // se o paramentro latest-pulmonar-indice for passado na request
+                // entao acrescenta na resposta o indice pulmonar mais recente
+                $pacientes = $pacientes->with('latestPulmonarIndice');
+        }
+        
+        $pacientes = $pacientes->get();
+        return PacienteResource::collection($pacientes);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PacienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['prefix' => 'paciente'], function () {
+    Route::get("index", [PacienteController::class, "index"]);
+    Route::get("index-by-date", [PacienteController::class, "indexByDate"]);
 });
